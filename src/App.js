a60b48d1ea65e43802d0ba6a4839f2bc557d5ed2
rishbitsbytes <rishrathore1613@gmail.com>
2022-05-06 11:35:18 +0530

    added: simple jsx practice

--- src/App.js
+++ src/App.js
@@ -2,22 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const pets = [
+    {
+      name: 'Dolly',
+      type: 'cat',
+      description: 'Dolly is funny and loves to be around people',
+      skills: ['fetch', 'play dead', 'roll over'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829788/cats/remi-remino-E9kVmtiqqGE-unsplash_uivwfy.jpg',
+    },
+    {
+      name: 'Molly',
+      type: 'cat',
+      description: 'Molly is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+    {
+      name: 'Molly1',
+      type: 'cat',
+      description: 'Molly is a sweet and cuddly cat stretch a lot!',
+      skills: ['play dead', 'meow', 'purr'],
+      image: 'https://res.cloudinary.com/atapas/image/upload/v1604829829/cats/timo-volz-ZlFKIG6dApg-unsplash_x3xifk.jpg',
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Pets</h1>
+      <ul className='container'>
+        {
+          pets.map((pet,index) => (
+            <li key={index}>
+            <h1>{pet.name}</h1>
+            <p>{pet.description}</p>
+             <img src={pet.image} 
+                alt={pet.name} 
+                width="200px"
+                height="200px" />
+                <ul className='skills'>
+                  {
+                    pet.skills.map((skill,index) => (
+                      <li key={skill}>{skill}</li>
+                      ))
+                  }  
+                </ul>
+            </li>
+          ))
+        }
+      </ul>
+
     </div>
   );
 }
